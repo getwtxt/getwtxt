b133debd9d6f7d80bfd0279dd1f7ef5259492a58
Ben Morrison <ben@gbmor.dev>
2019-05-11 03:33:12 -0400

    removed unneded conf field

--- types.go
+++ types.go
@@ -1,20 +1,13 @@
 package main
 
-import "regexp"
-
 const txtutf8 = "text/plain; charset=utf8"
 const htmlutf8 = "text/html; charset=utf8"
 
 type configuration struct {
 	port         string
-	validPath    *regexp.Regexp
 	quietLogging bool
 	fileLogging  bool
 	logFile      string
 }
 
 var confObj = &configuration{}
-
-func initConfig() {
-	confObj.validPath = regexp.MustCompile("^/(api)/(plain)/(tweets|users|tags|mentions)")
-}
